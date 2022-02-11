bd69017ed1002780e08cf52f334da74f2d727482
flowerYoungg <khy1896@gmail.com>
2022-02-11 23:08:41 +0900

    Feb11 S3C6

--- src/main/kotlin/section3/S3C5.kt
+++ src/main/kotlin/section3/S3C5.kt
@@ -6,20 +6,24 @@ package section3
 fun main(){
 
     val arr1 = arrayListOf<String>("1", "2")
-    val result1 = solution(arr1)
+    val result1 = solution1(arr1)
     println(result1)
     // 1.5
 
     val arr2 = arrayListOf<String>("1", "2", "3", "4","5")
-    val result2 = solution(arr2)
+    val result2 = solution1(arr2)
     println(result2)
     // 3.0
-
-
 }
 
 
-fun solution(arr: ArrayList<String>): Double {
+fun solution1(arr: ArrayList<String>) : Double{
+    var sum = 0
+    var length = arr.size
+    for(i in arr){
+        sum += Integer.parseInt(i)
+    }
+    val result = sum.toDouble()/length.toDouble()
 
-
+    return result
 }
