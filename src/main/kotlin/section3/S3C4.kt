92496a8520b3dc9f132e4e39e8243be78e2299a7
flowerYoungg <khy1896@gmail.com>
2022-02-11 22:18:32 +0900

    Feb11 S3C4

---
+++ src/main/kotlin/section3/S3C4.kt
@@ -0,0 +1,27 @@
+package section3
+
+// 별이 아래와 같이 2배씩 늘어나며 출력되는 solution을 완성하세요
+
+fun main(){
+
+    solution(3)
+    // *
+    // **
+    // ****
+
+    solution(5)
+    // *
+    // **
+    // ****
+    // ********
+    // ****************
+}
+
+fun solution(star : Int){
+    var res = 1
+    println("*")
+    for(i in 1..(star-1)){
+        res *= 2
+        println("*".repeat(res))
+    }
+}
