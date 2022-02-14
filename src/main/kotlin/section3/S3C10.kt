da8a0df714d02ceeb94014f39654526e411016ef
flowerYoungg <khy1896@gmail.com>
2022-02-14 19:22:23 +0900

    Feb14 S3C10

--- src/main/kotlin/section3/S3C10.kt
+++ src/main/kotlin/section3/S3C10.kt
@@ -4,19 +4,37 @@ package section3
 
 fun main(){
 
-    val result1 = solution("민수 : 3, 민지  : 4, 찬혁 : 1.5, 지우 : 2")
+    val result1 = solution10("민수 : 3, 민지  : 4, 찬혁 : 1.5, 지우 : 2")
     println(result1)
     // 찬혁
 
-    val result2 = solution("지영 : 3.7, 민영  : 1.2 , 해피 : 2.7, 퍼니 : 3")
+    val result2 = solution10("지영 : 3.7, 민영  : 1.2 , 해피 : 2.7, 퍼니 : 3")
     println(result2)
     // 민영
 
 }
 
 
-fun solution(str: String) : String{
+fun solution10(str: String) : String{
+    val result = str.split(",")
 
-    return ""
+    val nameList = mutableListOf<String>()
+    val distanceList = mutableListOf<Double>()
+
+    for(i in result){
+        val name = i.split(":")[0]
+        val distance = i.split(":")[1].toDouble()
+
+        nameList.add(name)
+        distanceList.add(distance)
+    }
+
+    //최소값 찾기
+    val minDistance = distanceList.minOrNull()
+    //최소값의 인덱스를 찾기
+    val resultIndex = distanceList.indexOf(minDistance)
+
+
+    return nameList[resultIndex].trim()
 }
 
